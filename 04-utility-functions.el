49805d0cb7bdbe6bd7f3c83dffaed54dac6f2946
Pieter Pareit <pieter.pareit@gmail.com>
2019-10-18 06:23:25 +0200

    Add more examples

--- 04-utility-functions.el
+++ 04-utility-functions.el
@@ -13,4 +13,45 @@
 
 (find2 '(lambda (x) (if (eq (car x) 'b) (cdr x))) '((a b c) (b c a) (c a b)))
 
+(cl-defun before (x y lst &key (test #'eql))
+  (and lst
+       (let ((first (car lst)))
+	 (cond ((funcall test y first) nil)
+	       ((funcall test x first) lst)
+	       (t (before x y (cdr lst) :test test))))))
+
+(ert-deftest test-before ()
+  (should (before 'b 'd '(a b c d)))
+  (should (not (before 'd 'b '(a b c d))))
+  (should (not (before 'e 'g '(a b c d)))))
+
+(cl-defun split-if (fn lst)
+  (let ((acc nil))
+    (do ((src lst (cdr src)))
+	((or (null src)
+	     (funcall fn (car src)))
+	 (cl-values (nreverse acc) src))
+      (push (car src) acc))))
+
+(ert-deftest test-split-if ()
+  (should (equal (split-if (lambda (x) (> x 3)) '(1 2 3 4 5))
+		 '((1 2 3) (4 5)))))
+
+
+(cl-defun map-> (fn start test-fn succ-fn)
+  (do ((i start (funcall succ-fn i))
+       (result nil))
+      ((funcall test-fn i)
+       (nreverse result))
+    (push (funcall fn i) result)))
+
+(ert-deftest test-map-> ()
+  (should (equal (map-> '(lambda (number) (% number 2))
+			1
+			'(lambda (number) (< 4 number))
+			'1+)
+		 '(1 0 1 0))))
+
+
+
 ;;; 04-utility-functions.el ends here
