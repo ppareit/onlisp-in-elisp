dc968a15d7262021b06c691f38ad86c80deb77b6
Pieter Pareit <pieter.pareit@gmail.com>
2020-02-14 06:06:57 +0100

    Using a compiled network

--- 06-functions-as-representation.el
+++ 06-functions-as-representation.el
@@ -62,4 +62,43 @@
 
 (funcall (gethash 'penny *nodes*))
 
+;;;---------------------------------
+
+(setq lexical-binding t)
+
+(defvar *nodes* nil)
+
+(defun defnode (&rest args)
+  "Define a node with the arguments ARGS."
+  (push args *nodes*)
+  args)
+
+(defnode 'people "Is the person a man? " 'male 'female)
+(defnode 'male "Is he alive? " 'liveman 'deadman)
+(defnode 'deadman "Was he American? " 'us 'them)
+(defnode 'us "Is he on a coin? " 'coin 'cidence)
+(defnode 'coin "Is the coin a penny? " 'penny 'coins)
+(defnode 'penny 'lincoln)
+
+(defun compile-net (root)
+  "Compile the network with root node ROOT."
+  (let ((node (assoc root *nodes*)))
+    (if (null node)
+	nil
+      (let ((conts (second node))
+	    (yes (third node))
+	    (no (fourth node)))
+	(if yes
+	    (let ((yes-fn (compile-net yes))
+		  (no-fn (compile-net no)))
+	      #'(lambda ()
+		  (funcall (if (y-or-n-p conts)
+			       yes-fn
+			     no-fn))))
+	  #'(lambda () conts))))))
+
+(setq network (compile-net 'people))
+
+(funcall network)
+
 ;;; 06-functions-as-representation.el ends here
